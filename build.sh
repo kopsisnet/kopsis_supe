c94c1c3fa1cd2c269e7e177ffa617df92515aead
kopsisnet <kopsis.net@outlook.com>
2025-09-08 22:18:35 +0300

    Fix deployment configuration and build script

--- build.sh
+++ build.sh
@@ -1,4 +1,16 @@
 #!/usr/bin/env bash
+set -o errexit  # Script'i herhangi bir hata durumunda durdur
 
+echo "Installing dependencies..."
+pip install -r requirements.txt
+
+echo "Creating static directory..."
+mkdir -p staticfiles
+
+echo "Collecting static files..."
 python manage.py collectstatic --no-input
+
+echo "Running database migrations..."
 python manage.py migrate
+
+echo "Build completed successfully!"
--- config/settings.py
+++ config/settings.py
@@ -80,12 +80,13 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+import dj_database_url
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'URL': os.getenv('DATABASE_URL'),
-        'CONN_MAX_AGE': 0,
-    }
+    'default': dj_database_url.config(
+        default='sqlite:///db.sqlite3',
+        conn_max_age=600
+    )
 }
  
 
@@ -124,6 +125,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
