6f986ad92d7850d76e5bf50dc673e7c5a11c1227
kopsisnet <kopsis.net@outlook.com>
2025-09-08 23:23:32 +0300

    Add homepage template and view configuration

--- config/settings.py
+++ config/settings.py
@@ -62,13 +62,20 @@ ROOT_URLCONF = 'config.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.debug',
+                'django.template.context_processors.i18n',
+                'django.template.context_processors.media',
+                'django.template.context_processors.static',
+                'django.template.context_processors.tz',
             ],
         },
     },
--- core/views.py
+++ core/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
 
-# Create your views here.
+class HomeView(TemplateView):
+    template_name = 'core/home.html'
+
+def home(request):
+    return render(request, 'core/home.html')
