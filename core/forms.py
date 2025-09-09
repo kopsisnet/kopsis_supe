885c9bc9faf408ebe841aa3590ac4e4449c781cf
kopsisnet <kopsis.net@outlook.com>
2025-09-09 10:04:01 +0300

    Update forms.py

--- core/forms.py
+++ core/forms.py
@@ -3,14 +3,16 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
 class UserRegistrationForm(UserCreationForm):
-    email = forms.EmailField(required=True, widget=forms.EmailInput(attrs={'class': 'form-control'}))
-    
+    email = forms.EmailField(required=True, widget=forms.EmailInput())
+
     class Meta:
         model = User
         fields = ["username", "email", "password1", "password2"]
-    
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        # Bootstrap class'larını ekleyelim
-        for field in self.fields:
-            self.fields[field].widget.attrs['class'] = 'form-control'
+        for field_name, field in self.fields.items():
+            css_class = 'form-control'
+            if self.errors.get(field_name):
+                css_class += ' is-invalid'
+            field.widget.attrs['class'] = css_class
