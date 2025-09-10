dc8c4985de4d865b438b25f8fef29f26507c8463
kopsisnet <kopsis.net@outlook.com>
2025-09-10 23:50:23 +0300

    Update 2025-09-10 23:50:23: config/urls.py

--- config/urls.py
+++ config/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('accounts/register/', register, name='register'),
     path('accounts/', include('django.contrib.auth.urls')),
     path('admin/', admin.site.urls),
+    path('personel/', include('personel.urls')),
 ]
